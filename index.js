2e5e49627517c42c4ceaa9049bb1c9a6a1e23316
belij2111 <belijvalerij2111@gmail.com>
2024-03-24 11:54:53 +0300

    add: Add a new entry to the branch dev2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.body.append(buttonStart)
 function start() {
     document.write(`branch-main <br>`)
     document.write(`branch-dev1 <br>`)
+    document.write(`branch-dev2 <br>`)
 }
 
 buttonStart.addEventListener('click', start)
