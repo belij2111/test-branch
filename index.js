61c0b99140c0f7863025e47cd8b277a31b8fb398
belij2111 <belijvalerij2111@gmail.com>
2024-03-24 11:43:52 +0300

    refactor: Insert a text wrap

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ buttonStart.append('start')
 document.body.append(buttonStart)
 
 function start() {
-    document.write("branch-main")
+    document.write(`branch-main <br>`)
 }
 
 buttonStart.addEventListener('click', start)
